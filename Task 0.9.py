a37c56f81d630c88391869cb94cfd98c3b5a9c79
Tammy <84852746+tamstraker@users.noreply.github.com>
2021-06-25 16:20:21 +0200

    Add files via upload
    Added missed file Task 09

---
+++ Task 0.9.py
@@ -0,0 +1,12 @@
+from typing import Counter
+
+
+def vowels(str):
+    vowels = ["A","a","E","e","I","O","o","U","u"]
+    str ="cat ran in a hat"
+    
+    for letter in str:
+        if vowels:
+            print(vowels)
+        else:
+            print(None)
